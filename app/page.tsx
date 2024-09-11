4ef6ccc5b07957b857e00534c4a2eb092b3454b3
Adrian Mørbán <46272302+adrianmorban@users.noreply.github.com>
2024-09-10 22:52:51 -0400

    Update app styling and add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,58 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faGithub, faLinkedin, faInstagram, faCodepen} from '@fortawesome/free-brands-svg-icons'; 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full flex justify-center px-24">
+      <div className="grid grid-cols-2 w-full max-w-screen-xl">
+        <div className="min-h-screen">
+          <div className="sticky top-0 h-screen pt-48 pb-20 flex flex-col justify-between">
+            <div>
+              <h1 className="text-7xl font-bold tracking-tight text-slate-200">Adrian Morban</h1>
+              <h2 className="text-2xl text-slate-400 font-semibold">Full-Stack Developer</h2>
+            </div>
+            <div className='flex text-slate-200 gap-x-4'>
+              <FontAwesomeIcon icon={faLinkedin} className='w-8 h-8' />
+              <FontAwesomeIcon icon={faGithub} className='w-8 h-8' />
+              <FontAwesomeIcon icon={faCodepen} className='w-8 h-8' />
+              <FontAwesomeIcon icon={faXTwitter} className='w-8 h-8' />
+              <FontAwesomeIcon icon={faInstagram} className='w-8 h-8' />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="text-slate-400 flex flex-col gap-4 text-lg">
+          <div className="py-48 flex flex-col gap-4">
+            <p>Soy un capacitado desarrollador <b>Full-Stack</b> con experiencia en desarrollo web y una
+                sólida formación técnica en Ingeniería de Sistemas. Competente en tecnologías como
+                Node.js, PHP, Laravel y más, me destaco en la creación de soluciones web adaptadas
+                a las diversas necesidades de los clientes.
+            </p>
+            <p>Mi experiencia incluye el desarrollo de páginas web, plataformas de comercio electrónico y herramientas web, con enfoque en optimización SEO y código de alta calidad.
+                Me dedico a entregar resultados que superen las expectativas del cliente.
+            </p>
+            <p>Mis habilidades interpersonales, incluyendo el liderazgo, la resolución de problemas y
+                el trabajo en equipo, complementan mis habilidades técnicas, lo que me convierte en
+                un activo valioso en entornos colaborativos. Estoy comprometido a mantenerme actualizado con las tendencias de la industria y contribuir a proyectos innovadores.
+            </p>
+          </div>
+          <div className="py-48 flex flex-col gap-4">
+            <p>Soy un capacitado desarrollador <b>Full-Stack</b> con experiencia en desarrollo web y una
+                sólida formación técnica en Ingeniería de Sistemas. Competente en tecnologías como
+                Node.js, PHP, Laravel y más, me destaco en la creación de soluciones web adaptadas
+                a las diversas necesidades de los clientes.
+            </p>
+            <p>Mi experiencia incluye el desarrollo de páginas web, plataformas de comercio electrónico y herramientas web, con enfoque en optimización SEO y código de alta calidad.
+                Me dedico a entregar resultados que superen las expectativas del cliente.
+            </p>
+            <p>Mis habilidades interpersonales, incluyendo el liderazgo, la resolución de problemas y
+                el trabajo en equipo, complementan mis habilidades técnicas, lo que me convierte en
+                un activo valioso en entornos colaborativos. Estoy comprometido a mantenerme actualizado con las tendencias de la industria y contribuir a proyectos innovadores.
+            </p>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
     </div>
   );
 }
